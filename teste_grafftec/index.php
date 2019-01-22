adcdbd5c7317d1498e5c363448e10132ecbc4203
Caio R. Gabardo Santar <33137447+kaioso20@users.noreply.github.com>
2019-01-22 01:18:47 -0200

    Addict new page for system control
    This index has been implemented for check DataBase in project

--- teste_grafftec/index.php
+++ teste_grafftec/index.php
@@ -1,3 +1,8 @@
 <?php
-    header("Location: Code/index.php");
+    include_once "DBlink/conexao.php";
+    if(!$conexao_fix){
+        die("O banco de dados não foi vinculado corretamente, não possui nenhum dado selecionado!");
+    }else{
+        header("Location: Code/index.php");
+    }
 ?>
